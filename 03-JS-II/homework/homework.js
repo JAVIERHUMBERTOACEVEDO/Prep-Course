8de1d44bb03824f3cb9e965b0832b55459316143
JAVIER HUMBERTO ACEVEDO GALLEGO <JAVIKARU@HOTMAIL.COM>
2022-11-11 17:46:36 -0500

    HOMEWORK 2 THIRTEEN EXERCISES OK

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -151,6 +151,20 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+
+ if ((num1===0)||(num2===0)||(num3===0))
+ {return 'Error'} 
+ else if ((num1<0)||(num2<0)||(num3<0))
+    {return 'Hay negativos'}
+  else if ((num1>num2)&&(num1>num3)&&(num1>0))
+    {return 'Número 1 es mayor y positivo'}
+  else if ((num3>num1)&&(num3>num2))
+    {return num3+1}
+  else
+    {return false}   
+
+
+
 }
 
 function esPrimo(numero) {
@@ -159,6 +173,25 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+
+var contador=0;
+
+if ((numero===0)||(numero===1))
+{return false;}
+
+for (let i=1;i<=numero;i++)
+{
+ if (numero%i===0)
+ {
+  contador++;
+ }
+}
+
+if (contador>2)
+{return false}
+else
+{return true}
+
 }
 
 function esVerdadero(valor){
@@ -166,6 +199,10 @@ function esVerdadero(valor){
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 
+  if (valor===true)
+    {return 'Soy verdadero';}
+  else if (valor===false)
+    {return 'Soy falso';}
 }
 
 function tablaDelSeis(){
