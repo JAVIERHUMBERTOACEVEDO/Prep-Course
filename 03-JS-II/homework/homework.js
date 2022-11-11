6abb740755a31971dc9412f4ffeb44bad9762875
JAVIER HUMBERTO ACEVEDO GALLEGO <JAVIKARU@HOTMAIL.COM>
2022-11-11 16:59:49 -0500

    HOMEWORK II SEVNE EXERCISES OK

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -105,6 +105,11 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+
+  if (numero>20 && numero<50)
+    {return true}
+  else
+    {return false}
 }
 
 function esEntero(numero) {
@@ -115,6 +120,12 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+
+  if (Math.floor(numero)===numero)
+    {return true}
+  else
+    {return false}
+
 }
 
 function fizzBuzz(numero) {
